64640582a3ab0e80256904b7e3aa1da956626b1b
wozzy84 <piotrw84@gmail.com>
2019-12-11 11:28:37 +0100

    sekcja z trzema kolumnami

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { HomeContainer } from '../HomeContainer/HomeContainer';
 import { HomeMainPic } from '../HomeMainPic/HomeMainPic';
 import {Test} from '../test'
+import { HomeThreeCol } from '../HomeThreeCol/HomeThreeCol';
 
 
 const Home = (props) => {
     return (
       <>
             <HomeContainer/>
+            <HomeThreeCol/>
             <Test/>
       </>      
           )
---
+++ src/components/HomeThreeCol/HomeThreeCol.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const HomeThreeCol = () => {
+    return (
+        <>
+            <section className="HomeThreeCol">
+                <div className="ThreeColContainer">
+                    <div className="column">
+                        <p className="column_nr">10</p>
+                        <p className="column_head">ODDANYCH WORKÓW</p>
+                        <p className="column_text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                    </div>
+                    <div className="column">
+                        <p className="column_nr">5</p>
+                        <p className="column_head">WSPARTYCH ORGANIZACJI</p>
+                        <p className="column_text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                    </div>
+                    <div className="column">
+                        <p className="column_nr">7</p>
+                        <p className="column_head">ZORGANIZOWANYCH ZBIÓREK</p>
+                        <p className="column_text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
+}
+
+
+export {HomeThreeCol}
+
+
