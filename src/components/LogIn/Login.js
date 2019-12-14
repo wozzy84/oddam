f6bedab654835ead1c6a4de34749d8c2d4cd9b9a
wozzy84 <piotrw84@gmail.com>
2019-12-14 23:25:20 +0100

    widok logowania

--- src/components/LogIn/Login.js
+++ src/components/LogIn/Login.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Login = () => {
-    return <h1>Logowanie</h1>
+
+
+    return  (
+        <>
+        
+        <section className="loginSection">
+            <h1>Zaloguj się</h1>
+            <div className="loginDecoration"></div>
+            <form className="loginForm">
+                <div className="loginInputGroup">
+                    <div className="loginInputContainer">
+                        <label htmlFor="loginEmail">Email</label>
+                        <input type="email" id="loginEmail"></input>
+                    </div>
+                    <div className="loginInputContainer">
+                        <label htmlFor="loginPasswd">Hasło</label>
+                        <input type="password"></input>
+                    </div>
+                    
+                </div>
+                <div className="loginButtonGroup">
+                    <Link to ='rejestracja'><button className="signInBtn" type="button">Załóż konto</button></Link>
+                    <button className="logInBtn" type="submit">Zaloguj się</button>
+                </div>
+
+            </form>
+
+        </section>
+
+        </>
+    )
 }
 
 export {Login}
