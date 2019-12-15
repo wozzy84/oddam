f3815ab20ad766cf85f31397aecaed8de3579225
wozzy84 <piotrw84@gmail.com>
2019-12-15 14:41:58 +0100

    widok wylogowania

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
     } from 'react-router-dom';
 import { Login } from './components/LogIn/Login';
 import { Register } from './components/register/register';
+import {LogOut} from './components/LogOut/LogOut'
 
 const App = (props) => {
 
@@ -18,6 +19,7 @@ return (
             <Route exact path= "/" component={Home} />
             <Route path ='/logowanie' component= {Login}/>
             <Route path = '/rejestracja' component={Register}/>
+            <Route path ='/wylogowano' component= {LogOut}/>
         </HashRouter>
 
     </>
---
+++ src/components/LogOut/LogOut.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const LogOut= () => {
+
+
+    return  (
+        <>
+        
+        <section className="logOutSection">
+            <h1>Wylogowanie nastąpiło <br/> pomyślnie!</h1>
+            <div className="logOutDecoration"></div>
+            <Link to ='/'><button className="mainPageBtn" type="button">Strona główna</button></Link>
+
+        </section>
+
+        </>
+    )
+}
+
+export {LogOut}
