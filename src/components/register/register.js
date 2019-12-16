f80b61d32a9f76bcac27c14f6078c2b3a6eca008
wozzy84 <piotrw84@gmail.com>
2019-12-16 10:51:05 +0100

    walidacja rejestracji

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,7 +1,64 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {useState, useEffect} from 'react';
 
 const Register = () => {
+    const [emailError, setEmailError] = useState(false);
+    const [passwdError, setPasswdError] = useState(false); 
+    const redBar = {borderBottom: "1px solid #D8000C"};
+    const blackBar = {borderBottom: "1px solid   #3C3C3C"};  
+    const [email, setEmail] = useState("");
+    const [passwd, setPasswd] = useState("");
+    const [passwdRepeat, setPasswdRepeat] = useState('')
+    const [submitTry, setSubmitTry] = useState(false)
+
+    const handleChange = (e) => {
+        switch (e.currentTarget.id){
+            case "registerEmail":
+            setEmail(e.currentTarget.value)
+            return
+            case "registerPasswd":
+            setPasswd(e.currentTarget.value)
+            return
+            case "registerPasswdrepeat":
+            setPasswdRepeat(e.currentTarget.value)
+            return
+        }
+    }
+
+    const validateEmail = (e) =>{
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(e).toLowerCase());
+    }
+
+    const validatePasswd = (e) => {
+        if (passwd==passwdRepeat) {
+            return true
+        };
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        const emailValidatiton = validateEmail(email);
+        const passwdValidaion = validatePasswd(passwd);
+        setEmailError(!emailValidatiton);
+        setPasswdError(!passwdValidaion);
+        if (submitTry==false) {
+            setSubmitTry(true)
+        }
+        
+    }
+
+    useEffect(()=> {
+        const emailValidatiton = validateEmail(email);
+        const passwdValidaion = validatePasswd(passwd);
+        if (submitTry==true) {
+             setEmailError(!emailValidatiton);
+             setPasswdError(!passwdValidaion);
+        }
+        
+       
+    },[email, passwd, passwdRepeat ])
 
 
     return  (
@@ -14,21 +71,24 @@ const Register = () => {
                 <div className="registerInputGroup">
                     <div className="registerInputContainer">
                         <label htmlFor="registerEmail">Email</label>
-                        <input type="email" id="registerEmail"></input>
+                        <input type="email" id="registerEmail" style={emailError? redBar: blackBar} value={email} onChange={handleChange}></input>
+                        <p className="errorMsg" style={emailError? {opacity: "1"}: {opacity: 0}}>Niepoprawny email!</p>
                     </div>
                     <div className="registerInputContainer">
                         <label htmlFor="registerPasswd">Hasło</label>
-                        <input type="password"></input>
+                        <input type="password" id="registerPasswd" style={passwdError? redBar: blackBar} value={passwd} onChange={handleChange}></input>
+                        <p className="errorMsg" style={passwdError? {opacity: "1"}: {opacity: 0}} >Podane hasła są różne</p>
                     </div>
                     <div className="registerInputContainer">
                         <label htmlFor="registerPasswd">Powtórz hasło</label>
-                        <input type="password"></input>
+                        <input type="password" id="registerPasswdrepeat" style={passwdError? redBar: blackBar} value={passwdRepeat} onChange={handleChange}></input>
+                        <p className="errorMsg" style={passwdError? {opacity: "1"}: {opacity: 0}} >Podane hasła są różne</p>
                     </div>
                     
                 </div>
                 <div className="registerButtonGroup">
                     <Link to ='/logowanie'><button className="RegLogInBtn" type="button">Zaloguj się</button></Link>
-                    <button className="RegSignInBtn" type="submit">Załóż konto</button>
+                    <button className="RegSignInBtn" type="submit" onClick={handleClick}>Załóż konto</button>
                 </div>
 
             </form>
